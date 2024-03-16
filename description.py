e28c757c3dcdb91dbf0c86e8ab84d56af7cd7af1
Kartikey_Pandey1884 <51755548+kartikey-onlineGOD@users.noreply.github.com>
2024-03-16 15:50:46 -0400

    First Commmit, Web Scraper and Description to Keyword done

---
+++ description.py
@@ -0,0 +1,28 @@
+
+api_key = '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'
+
+from openai import OpenAI
+import re
+client = OpenAI(api_key=api_key)
+
+def check(prompt):
+    completion = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": "You are a Hackathon Judge that can read the description that I give you and give me a list of keywords that would be useful to find other super similar projects, so no common AI words, super specific stuff."},
+        {"role": "user", "content": prompt}
+    ]
+    )
+
+    
+    keywords_message = completion.choices[0].message.content
+    keywords_list = keywords_message.split(".")
+    keywords_list = [re.sub(r'\d+\s*', '', keyword).strip() for keyword in keywords_list]
+
+
+    return keywords_list
+
+
+
+
+
---
+++ web_scraper.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+
+def scrape_devpost(link):
+    # Send a GET request to the provided link
+    print("Entered")
+    response = requests.get(link)
+    print(response.status_code)
+
+    # Extract project title
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title_elem = soup.find('h1')
+        title = title_elem.get_text(strip=True) if title_elem else None
+
+        description = ''
+        details_container = soup.find(id='app-details-left')
+        if details_container:
+            for heading in details_container.find_all('h2'):
+                description += heading.get_text(strip=True) + '\n'
+                next_element = heading.find_next_sibling()
+                while next_element and next_element.name != 'h2':
+                    if next_element.name == 'p':
+                        description += next_element.get_text(strip=True) + '\n'
+                    next_element = next_element.find_next_sibling()
+
+
+        project_details = {'title': title, 'description': description.strip()}
+
+        return project_details
+    else:
+        print("Failed to retrieve data. Status code:", response.status_code)
+        return None
+
+# Example usage:
+devpost_link = "https://devpost.com/software/hackanalyzer"
+projects_data = scrape_devpost(devpost_link)
+print(projects_data)
